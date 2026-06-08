cf73bc60af0d30ca779fcfd63bef49abcd4186dc
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-06-08 09:46:33 +0530

    playing with route handlers

--- src/app.js
+++ src/app.js
@@ -2,21 +2,35 @@ const express = require("express");
 
 const app= express();
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"sahana",lastName:"Shetty"});
-});
-app.post("/user",(req,res)=>{
-    console.log("save data to the database")
-    res.send("data sucessfully saved to database");
-});
+// app.get("/user",(req,res)=>{
+//     res.send({firstName:"sahana",lastName:"Shetty"});
+// });
+// app.post("/user",(req,res)=>{
+//     console.log("save data to the database")
+//     res.send("data sucessfully saved to database");
+// });
 
-app.delete("/user",(req,res)=>{
-    res.send("data deleted")
-})
+// app.delete("/user",(req,res)=>{
+//     res.send("data deleted")
+// })
 
-app.use("/user",(req,res)=>{
+app.use("/user",
+    (req,res,next)=>{
+    console.log("1st handler")
+    
+    next();
+    // res.send("1st handler");
+}, 
+(req,res,next)=>{
     console.log("save data to the database")
-    res.send("hahaha");
+    // res.send("2nd handler");
+    next();
+}
+, 
+(req,res,next)=>{
+    console.log("3rd handler")
+    res.send("3nd handler");
+    // next();
 })
 
 app.listen(3,()=>{
