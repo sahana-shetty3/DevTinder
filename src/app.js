5fb0b681803d2376f15ff3809193ec221a6b019a
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-06-11 21:46:20 +0530

    patch and delete API

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const User=require("./models/user");
 
 app.use(express.json());
 
-
 app.post("/signup",async(req,res)=>{
     
     //creating a new instance of User model
@@ -24,7 +23,7 @@ app.get("/user",async (req,res)=>{
     const userEmail = req.body.emailId;
 
     try{
-       const users = await User.findOne({emailId:userEmail});
+       const users = await User.findOne({});
 
        if(users.length===0){
         res.status(404).send("user not found")
@@ -32,6 +31,7 @@ app.get("/user",async (req,res)=>{
        else{
           res.send(users);
     }
+    
     }
     catch(err){
         res.status(400).send("something went wrong");
@@ -47,6 +47,46 @@ app.get("/feed",async (req,res)=>{
     }
     catch(err){
         res.status(401).send("something went wrong")
+    }
+})
+//delete the data of the user
+app.delete("/user", async (req,res) =>{
+
+    const userId = req.body.userId;
+
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.status(401).send("user deleted sucessfully")
+    }
+    catch(err){
+        res.status(401).send("somehing went wrong")
+    }
+});
+//update the user
+
+app.patch("/user",async (req,res)=>{
+    
+    const userId= req.body.userId;
+    
+    const data = req.body;
+    
+
+    try{
+        const user=  await User.findByIdAndUpdate(userId,data,{returnDocument:"after"});
+        console.log(user);
+        res.send("user added sucessfully");
+        console.log(users);
+
+    }catch (err){
+        res.status(401).send("something went wrong");
+    }
+})
+
+app.("/helper", async (res,req)=>{
+    const name = req.body.firstName;
+
+    try{
+        const user = await User.findOne({})
     }
 })
     
