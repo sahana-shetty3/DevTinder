952bd3721a688363e775d33070c4822497b5131e
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-06-11 18:46:45 +0530

    GET API

--- src/app.js
+++ src/app.js
@@ -18,11 +18,38 @@ app.post("/signup",async(req,res)=>{
         res.status(401).send("error saving the user"+err.message);
 
     }
-    
-
 })
 
+app.get("/user",async (req,res)=>{
+    const userEmail = req.body.emailId;
 
+    try{
+       const users = await User.findOne({emailId:userEmail});
+
+       if(users.length===0){
+        res.status(404).send("user not found")
+       }
+       else{
+          res.send(users);
+    }
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
+
+app.get("/feed",async (req,res)=>{
+
+    try{
+        const users = await User.find({});
+        res.send(users)
+
+    }
+    catch(err){
+        res.status(401).send("something went wrong")
+    }
+})
+    
 
 connectDB().then(()=>{
     console.log("database connection sucessfull");
