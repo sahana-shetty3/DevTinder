c04d0dfc88c404521a73544b71450be53a06e644
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-06-27 21:23:58 +0530

    sending a connection request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,16 +2,34 @@ const express = require("express");
 const User=require("../models/user");
 const requestRouter = express.Router();
 const {userAuth}=require("../middlewares/auth");
+const ConnectionRequestModel =require("../models/connectionRequest")
 
-requestRouter.post("/sentConnectionReq",userAuth,async(req,res)=>{
-    try{
-        console.log("connection request is sent")
-        const user =req.user;
-        res.send(user.firstName + " connection request is sent")
+requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
+try{
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
+
+    const allowedStatus = ["ignored","intrested"];
+
+    if(!allowedStatus.includes(status))
+    {
+       return res.status(401).json({message:"Invalid status type : "+status})
     }
-    catch(err){
+
+    const connectionRequest = new ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status
+    });
+
+    const data = await connectionRequest.save();
+
+    res.json({message:"Connection request is sent sucessfully",data});
+}
+catch(err){
         res.status(401).send("Error: "+err.message);
-    }
+}
 })
 
 module.exports=requestRouter;
