22cf9c59306ff2e8e84dee7cdaa4906c2045d711
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-10 20:26:42 +0530

    updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -55,15 +55,5 @@ authRouter.post("/login",async (req,res)=>{
     }
 })
 
-authRouter.post("/logout",async (req,res)=>{
-    try{
-        res.cookie("token",null,{
-            expires:new Date(Date.now())
-        })
-        res.send("logout sucessfully");
-    }
-    catch(err){
-        res.status(401).send("error: "+err.message);
-    }
-})
+
 module.exports = authRouter;
